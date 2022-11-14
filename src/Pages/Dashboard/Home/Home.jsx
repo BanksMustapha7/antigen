82348b1293671f72a746cf0f8e6a7cf964c936e3
BanksMustapha7 <banksola05@gmail.com>
2022-11-14 03:04:31 +0100

    Mobile Dashboard Home

--- src/Pages/Dashboard/Home/Home.jsx
+++ src/Pages/Dashboard/Home/Home.jsx
@@ -15,15 +15,47 @@ function Home() {
   const user = "John";
   return (
     <div className="home">
-      <div className="card greet" style={{ background: "#fff" }}>
-        <div>
-          <h2>
-            {greeting}, <span>{user}</span>
-          </h2>
-          <p>Welcome to Antigen!</p>
-          <button>Chat with a doctor</button>
+      <div className="top">
+        <div className="box1">
+          <div className="greeting">
+            <div className="card greet" style={{ background: "#fff" }}>
+              <div>
+                <h2>
+                  {greeting}, <span>{user}</span>
+                </h2>
+                <p>Welcome to Antigen!</p>
+                <button>Chat with a doctor</button>
+              </div>
+              <img src={Welcome} alt="welcome" srcset="" />
+            </div>
+          </div>
+
+          <div className="vaccineCount">
+            <h3>Vaccination Overview</h3>
+            <div className="boxes">
+              <div
+                className="card count"
+                style={{ backgroundColor: "#084482" }}
+              >
+                <h4>TOTAL VACCINES GOTTEN</h4>
+                <h3>14</h3>
+              </div>
+              <div
+                className="card count"
+                style={{ backgroundColor: "#003265" }}
+              >
+                <h4>NUMBER OF PENDING VACCINES</h4>
+                <h3>7</h3>
+              </div>
+              <div className="card count" style={{ background: "#042241" }}>
+                <h4>SUCCESS VACCINATION</h4>
+                <h3>7</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <img src={Welcome} alt="welcome" srcset="" />
+
+        <div className="box 2"></div>
       </div>
     </div>
   );
