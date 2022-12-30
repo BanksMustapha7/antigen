fff85f28283b97628044f433d0b85ec06588047d
BanksMustapha7 <banksola05@gmail.com>
2022-12-30 03:04:11 +0100

    Updates
    Completed sidebar links
    Started Schedule Page

--- src/Pages/Dashboard/Schedule/Schedule.jsx
+++ src/Pages/Dashboard/Schedule/Schedule.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Schedule() {
-  return <div>Schedule</div>;
+  return (
+    <div>
+      <div className="tabs">
+        <button>VACCINATION</button>
+        <button>MEDICATION</button>
+      </div>
+
+      <div className="hospital">
+        <div className="box">
+          <h3>Hospital</h3>
+          <small>Show favorite hospital</small>
+        </div>
+
+        <input type="search" name="hospitals" id="" />
+      </div>
+    </div>
+  );
 }
 
 export default Schedule;
