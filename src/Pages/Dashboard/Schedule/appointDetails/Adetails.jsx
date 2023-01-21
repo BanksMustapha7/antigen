88850c3e0761365cf3b2c38ee09155a24c87051c
BanksMustapha7 <banksola05@gmail.com>
2023-01-21 21:54:34 +0100

    Appointment details
    Completed Appointed Details desktop UI

--- src/Pages/Dashboard/Schedule/appointDetails/Adetails.jsx
+++ src/Pages/Dashboard/Schedule/appointDetails/Adetails.jsx
@@ -1,65 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
 import "./adetails.css";
+import Edit from "../../../../assets/edit.png";
 
 function Adetails() {
-  const [success, setSuccess] = useState(false);
   return (
     <div className="adetails">
-      {success === false && (
-        <div>
-          <div className="top">
-            <h2>Appointment Details</h2>
-            <small>Your appointment details are shown here.</small>
+      <div className="top">
+        <h2>Appointment Details</h2>
+        <small>Your appointment details are shown here.</small>
+      </div>
+      <div className="bodyy">
+        <div className="body_tile">
+          <div className="">
+            <h4>Hospital Name</h4>
+            <small>LASUTH</small>
           </div>
-          <div className="bodyy">
-            <div className="body_tile">
-              <div className="">
-                <h4>Hospital Name</h4>
-                <small>LASUTH</small>
-              </div>
 
-              <button>edit</button>
-            </div>
-
-            <div className="body_tile">
-              <div className="">
-                <h4>Vaccination Type</h4>
-                <small>COVID-19 Vaccine</small>
-              </div>
-
-              <button>edit</button>
-            </div>
-
-            <div className="body_tile">
-              <div className="">
-                <h4>Patient Name</h4>
-                <small>Joshua Jide</small>
-              </div>
-
-              <button>edit</button>
-            </div>
-            <div className="body_tile">
-              <div className="">
-                <h4>Appointment Date</h4>
-                <small>8th of August, 2022</small>
-              </div>
-
-              <button>edit</button>
-            </div>
-            <div className="body_tile">
-              <div className="">
-                <h4>Time</h4>
-                <small>9:00Am</small>
-              </div>
-
-              <button onClick={setSuccess(true)}>edit</button>
-            </div>
-
-            <button className="button">Book Appointment</button>
-          </div>
-          || (<div>Success!!</div>)
+          <button className="edit">
+            <img src={Edit} alt="edit" />
+          </button>
         </div>
-      )}
+
+        <div className="body_tile">
+          <div className="">
+            <h4>Vaccination Type</h4>
+            <small>COVID-19 Vaccine</small>
+          </div>
+
+          <button className="edit">
+            <img src={Edit} alt="edit" />
+          </button>
+        </div>
+
+        <div className="body_tile">
+          <div className="">
+            <h4>Patient Name</h4>
+            <small>Joshua Jide</small>
+          </div>
+
+          <button className="edit">
+            <img src={Edit} alt="edit" />
+          </button>
+        </div>
+        <div className="body_tile">
+          <div className="">
+            <h4>Appointment Date</h4>
+            <small>8th of August, 2022</small>
+          </div>
+
+          <button className="edit">
+            <img src={Edit} alt="edit" />
+          </button>
+        </div>
+        <div className="body_tile">
+          <div className="">
+            <h4>Time</h4>
+            <small>9:00Am</small>
+          </div>
+
+          <button className="edit">
+            <img src={Edit} alt="edit" />
+          </button>
+        </div>
+
+        <button className="book">Book Appointment</button>
+      </div>
     </div>
   );
 }
